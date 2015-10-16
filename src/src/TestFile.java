a1d1fd290c3237e9ae6ab0c08ca286efd7537799
brett.a.smith@outlook.com <brett.a.smith@outlook.com>
2015-10-16 11:14:12 +1100

    Fixed bug 2 where the player never reaches their betting limit in the application. Supplied the required output files and included the details in the debugging log.

--- src/src/TestFile.java
+++ src/src/TestFile.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class TestFile {
     public static void main(String[] args)
     {
-        runOddsCheck();
+        runWinBettingLimitTest();
     }
     
     public static void runWinPayoutTest(DiceValue pick)
